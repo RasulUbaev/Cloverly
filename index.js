73de83f7e02a31d4751c9e3073161a490c12c40e
RasulUbaev <ubaevrasul@gmail.com>
2022-11-26 14:29:38 +0500

    cloverly

---
+++ index.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "bg-green": "#1FBD8A",
+        "bg-yellow-200": "#F2F4E8",
+        "bg-yellow-100": "#FBFCF5",
+      },
+      fontSize: {
+        "title-h1": "60px",
+        subtitle: "18px",
+      },
+      spacing: {
+        xl: "19px",
+      },
+      borderRadius: {
+        xxl: "50px",
+      },
+      fontFamily: {
+        roboto: ["Roboto", sans - serif],
+      },
+    },
+  },
+};
